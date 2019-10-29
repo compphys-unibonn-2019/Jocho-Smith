d6adf5f3c5787a276b652feacecd24c1ce3b688e
Jocho-Smith <56628013+Jocho-Smith@users.noreply.github.com>
2019-10-29 09:18:09 +0100

    monte carlo mean calculation of random walk
    this code is not optimized, since all is done with for loops.

--- exercise_1/random_walk.R
+++ exercise_1/random_walk.R
@@ -1,5 +1,39 @@
 ## Random Walk
 
-# start at the origen
-x <- c(0)
-y <- c(0)
+
+
+random_step <- function(l){
+  ## makes one random step in a 2D grid
+  x = l[1]
+  y = l[2]
+  # generate random number 
+  a = round(runif(1))
+  choice = (-1)^a
+  a = round(runif(1))
+  pm = (-1)^a
+  if (choice==1){
+    x = x + pm
+  } else {
+    y = y + pm
+  }
+  return(c(x,y))
+}
+
+random_walk <- function(N){
+  ## makes N random step in a 2D grid
+  l = c(0,0)
+  
+  for (i in 1:N){
+    l = random_step(l)
+  }
+  return(l)
+}
+dist = 0
+n = 1000
+for (i in 1:n){
+  dist = dist + sum(random_walk(n))
+  avrg = dist/n # this is than the average distance from the origen after a random walk with n steps
+}
+print(avrg)
+## this code runs about 12 seconds on my machine 
+## in order to increase the speed one coule use less for loops and more vector operations. For example one could create a list of all steps in one direction and dann just sum up the entire list. 
